6ec3b95390005162c4e5d183c23c3449ee791a71
Kevin Moye <kevin.moye@outlook.com>
2020-03-23 09:28:31 -0400

    adding EF code... retry

---
+++ fishRepo_EF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fishRepo_EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
